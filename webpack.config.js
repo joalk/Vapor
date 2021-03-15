9a27857a0c0c4c0d0149fd2036748e0362f74af4
joalk <joalkims@gmail.com>
2021-03-15 15:58:20 -0700

    created webpack and installed webpack loaders

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './Frontend/index.js',
+  output: {
+    path: path.join(__dirname, './bundle'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    publicPath: '/bundle',
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+};
