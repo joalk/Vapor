19770a99bee43a964834e5f867c4a841116ddfb9
joalk <joalkims@gmail.com>
2021-03-17 21:55:47 -0700

    changed weeks to 7 days

--- Frontend/components/Week.jsx
+++ Frontend/components/Week.jsx
@@ -3,7 +3,7 @@ import { Day } from './Day.jsx'
 
 export const Week = () => {
     let days = [];
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 8; i++) {
         days.push(<Day />)
     }
     return (
