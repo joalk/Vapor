848236b769f9819b46068f6b61a9efab0e8c50d7
joalk <joalkims@gmail.com>
2021-03-17 22:24:08 -0700

    added an increase function

--- Frontend/components/AverageDisplay.jsx
+++ Frontend/components/AverageDisplay.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export const AverageDisplay = () => {
+    // const increase = () => {
+    //     if {}
+    // }
+    
     return (
         <div className="display">
             <h1>Congratulations, your puff count has decreased by []%</h1>
