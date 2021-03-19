ae1877dd98cc456986e21b3ecc0041493b9ccde0
joalk <joalkims@gmail.com>
2021-03-18 18:33:03 -0700

    organized parent and child components for smoothe data flow

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AverageDisplay } from './components/AverageDisplay.jsx';
 import { Container } from './components/Container.jsx'
-import { PuffCreator } from './components/PuffCreator.jsx';
 
 class App extends React.Component {
 	// name property goes here:
@@ -14,10 +13,7 @@ get name() {
     return (
         <div className="app">
             <h1>My name is {this.name}.</h1>
-            <div className="panel">
               <Container />
-              <PuffCreator />
-            </div>
               <AverageDisplay />
         </div>
     )
--- Frontend/components/Container.jsx
+++ Frontend/components/Container.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Month } from './Month.jsx'
+import { PuffCreator } from './PuffCreator.jsx';
 
-export const Container = () => {
+export class Container extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            puffValue: 0,
+            puffArray: []
+        }
+        this.puffChange = this.puffChange.bind(this);
+        this.puffSubmit = this.puffSubmit.bind(this);
+    }
+
+    puffChange (e) {
+        this.setState({puffValue: e.target.value});
+    }
+
+    puffSubmit (e) {
+        e.preventDefault();
+        this.setState({
+            puffArray: puffArray.push(e.target.value)
+        })
+    }
+
+    render() {
     return (
         <div className="container">
             <h1>Hello</h1>
+            {this.state.puffArray}
             <Month />
+            <PuffCreator />
         </div>
     )
+    }
 }
